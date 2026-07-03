3e2b35988c2039e6f4006f1ca86df6b46e25ff0b
shaikh-kashish-bca <shaikhkashish183@gmail.com>
2026-07-03 09:57:09 +0530

    Day_2_sum_two_numbers.c

---
+++ sum_two_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    
+    printf("Day 2 of 365-basics\n");
+    printf("Learning C Basics for TCS\n\n");
+    
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    
+    sum = a + b;
+    
+    printf("\n----------------------\n");
+    printf("Sum = %d\n", sum);
+    printf("----------------------\n");
+    printf("Day 2 Completed\n");
+    
+    return 0;
+}
