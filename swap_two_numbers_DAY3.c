6064fad916812a1c61085914a9946e7d62974ad1
shaikh-kashish-bca <shaikhkashish183@gmail.com>
2026-07-05 16:11:47 +0530

    Swap_two_numbers_DAY3.c

---
+++ swap_two_numbers_DAY3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    // SWAPPING LOGIC
+    temp = a; 
+    a = b;     
+    b = temp;  
+
+    printf("After Swap: a = %d, b = %d", a, b);
+
+    return 0;
+}
