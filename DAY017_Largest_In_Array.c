8bb3d65c1d31c92be6706af7ef0f499acc69fdce
shaikh-kashish-bca <shaikhkashish183@gmail.com>
2026-08-09 16:58:25 +0530

    Create DAY017_Largest_In_Array.c

---
+++ DAY017_Largest_In_Array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int arr[5], max;
+    printf("Enter 5 numbers: ");
+    for(int i = 0; i < 5; i++) scanf("%d", &arr[i]);
+
+    max = arr[0];
+    for(int i = 1; i < 5; i++) {
+        if(arr[i] > max) max = arr[i];
+    }
+    printf("Largest = %d", max);
+    return 0;
+}
