7620fb1be6c39dcc99477a84091dc2911a448f77
shaikh-kashish-bca <shaikhkashish183@gmail.com>
2026-08-09 17:01:23 +0530

    Create DAY020_Function_Add.c

---
+++ DAY020_Function_Add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int add(int a, int b) {
+    return a + b;
+}
+int main() {
+    int x, y;
+    printf("Enter 2 numbers: ");
+    scanf("%d %d", &x, &y);
+    printf("Sum = %d", add(x, y));
+    return 0;
+}
