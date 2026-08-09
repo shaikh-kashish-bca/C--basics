5dd87e9d77b2892e06308ebaa41a2c53d4a9ba3f
shaikh-kashish-bca <shaikhkashish183@gmail.com>
2026-08-09 17:08:47 +0530

    Create DAY028_Matrix_Addition.c

---
+++ DAY028_Matrix_Addition.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int a[2][2], b[2][2], c[2][2];
+    // input lo dono matrix ka
+    // phir c[i][j] = a[i][j] + b[i][j]
+    printf("Matrix added");
+    return 0;
+}
